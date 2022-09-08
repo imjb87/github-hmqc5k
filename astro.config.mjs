e0496f0fe2b3e66dd39ba3645679a07a31a23976
imjb87 <46000619+imjb87@users.noreply.github.com>
2022-09-08 14:19:31 +0100

    2 files modified, 3 deleted

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [
-    svelte(),
-    vue(),
-    react(),
     storyblok({
       accessToken: 'x1lzDjnWQE3v0QvMm7DziQtt',
       components: {
